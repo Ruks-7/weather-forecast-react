cd2ea747367f17523939cce2eb901d79f1070bf5
Rukwaro <irenerukwaro9@gmail.com>
2024-06-12 13:34:15 +0300

    Added bootstrap

--- src/Details.js
+++ src/Details.js
@@ -6,10 +6,11 @@ import "./Details.css";
 export default function Details(){
     return(
         <div>
+            <div className="container">
         <h1>Nairobi,
             <small>Kenya</small>
             </h1>
-
+            </div>
                 <p className="weatherState">Mostly Rainy</p>
                     <div className="degree">20°
                     <div className="icon">🌧</div>
@@ -17,18 +18,41 @@ export default function Details(){
 
                     <p className="dayDetails">Wednesday, 6 June | 17:24pm</p>
 
-                    <div className="today">
-                    <Info icon="💧" measurement="60%" component="Humidity" />
-                    <Info icon="💨" measurement="2km/h" component="WindSpeed" />
-                    <Info icon="☔" measurement="30%" component="Precipitation" />
+                    <div className="today container">
+                        <div className="row">
+                            <div className="col"> 
+                            <Info icon="💧" measurement="60%" component="Humidity" />
+                            </div>
+                            <div className="col"> 
+                            <Info icon="💨" measurement="2km/h" component="WindSpeed" />
+                            </div>
+                            <div className="col"> 
+                            <Info icon="☔" measurement="30%" component="Precipitation" />
+                            </div>
+                    </div>
                     </div>
 
 
-                <div className= "forecastDays">
-                <Forecast day="Tue" temp="20°/25°"/>
-                <Forecast day="Wed" temp="18°/23°"/>
-                <Forecast day="Thur" temp="21°/25°"/>
-                <Forecast day="Fri" temp="19°/24°"/>
+                <div className= "container">
+                    <div className="row w-50%">
+
+                    <div className="col-sm">
+                        <Forecast day="Tue"  temp="20°/25°"/>
+                        </div>
+                    
+                    <div className="col-sm">
+                    <Forecast day="Wed"  temp="18°/23°"/>
+                        </div>
+
+                    <div className="col-sm">
+                        <Forecast day="Thur" temp="21°/25°"/>
+                        </div>
+
+                    <div className="col-sm">
+                        <Forecast day="Fri"  temp="19°/24°"/>
+                        </div>
+
+                    </div>
                 </div>
         </div>
     );
--- src/Search.js
+++ src/Search.js
@@ -3,7 +3,7 @@ import "./Search.css";
 
 export default function Search(){
     return (
-    <div>
+    <div className="container">
         <form>
             <input className="searchBar" type="search" placeholder="Enter a place..."></input>
             <input className="submit" type="submit" value="Search"></input>
